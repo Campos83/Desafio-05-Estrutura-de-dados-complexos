76949b7d868f7709013d3e99442712370ddfb9d1
Willian Campos <Campos83>
2022-07-08 11:02:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+ let itens = ["Gabigol", "Arrascaeta", "João Gomes", "Rodrigo Caio", "Pedro"]
+ console.log(itens)
+
+ itens.unshift("Claúdia");
+ console.log(itens)
+
+ itens.splice(5,1)
+ console.log(itens)
+
+itens.push("Roberta", "Willian")
+console.log(itens)
+
+itens.shift()
+console.log(itens)
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+console.log(numbers.sort())
